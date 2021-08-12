0c8a120542682fb487c341adb64cf8ef4a8c95eb
signorellidevelopers <66884792+signorellidevelopers@users.noreply.github.com>
2021-08-12 11:25:07 -0300

    logica laço de repetição

--- obj/aula-3-2while.cpp
+++ obj/aula-3-2while.cpp
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include<stdlib.h>
 int main(){
-	int x =1, n, a, soma=0 ;
-    printf("")
-	while(x<=5){
-		printf("Numero: %d \n",x);
+	int x=1, n, a, soma=0;
+    printf("Digite quantos numeros deseja somar");
+	scanf("%d", &n);
+	while(x<=n){
+		printf("Numero: %d ",x);
+	scanf("%d", &a);
+		soma = soma + a;
 		x++;
 	}
+	printf("\n valor toal da soma = %d", soma);
 	system("pause>>null");
 }
