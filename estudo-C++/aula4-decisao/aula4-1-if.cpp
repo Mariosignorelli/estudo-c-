a30966ccbff20d6a9b18313cad6223ba8136fdc6
signorellidevelopers <66884792+signorellidevelopers@users.noreply.github.com>
2021-08-12 21:18:30 -0300

    atualizado

---
+++ estudo-C++/aula4-decisao/aula4-1-if.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+   char a, b;
+      printf("\nEntre  com 2 letras:");
+      scanf("%c %c ", &a, &b);
+   if (a < b)
+   
+     printf("\n %c %c \n\n",a, b);
+
+      else
+      printf("\n %c %c \n\n",b, a);
+      
+system("pause");
+}
