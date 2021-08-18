e930574f5d42c01f22b34b7bf6726cbda9815270
signorellidevelopers <66884792+signorellidevelopers@users.noreply.github.com>
2021-08-18 08:41:19 -0300

    C++ atualizado

---
+++ estudo-C++/aula4-decisao/aula5-wich-for-do-while/aula5-while.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+main()
+{
+    int i;
+   
+printf("\n\n Os numeros pares de 0 a 100\n\n");
+while(i <=100) {
+if(i % 2 == 0)
+printf(" %d ",i);
+i++; // é o mesmo que i = i+1;        
+        }   
+        printf("\n\n fim\n\n");
+          system("pause");
+
+    }
+  
--- estudo-C++/aula4-decisao/teste.cpp
+++ estudo-C++/aula4-decisao/teste.cpp
@@ -1,6 +1,13 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main(){
-    printf("hello,Word.\n ");
+#include <stdio.h>
+main()
+{
+    int i;
+    printf("\n Os numeros naturais de 0 a 100\n\n");
+    i=0;
+    do {
+       printf("%d",i);
+       i++;    
+    } while(i<=100); 
+printf("\n\n fim \n\n");
+    system("pause");  
 }
- 
