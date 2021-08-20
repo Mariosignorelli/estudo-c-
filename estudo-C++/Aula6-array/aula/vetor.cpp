0f1a897fb47af1005ada78f136c9be238d98e1f7
signorellidevelopers <66884792+signorellidevelopers@users.noreply.github.com>
2021-08-20 20:40:48 -0300

    atualizado

--- estudo-C++/Aula6-array/aula/vetor.cpp
+++ estudo-C++/Aula6-array/aula/vetor.cpp
@@ -2,22 +2,22 @@
 #include <stdlib.h>
 main()
 {
-    int d, i, j, n, resto[64];
+    int d, i, j, n, restos[64];
     printf("\n\nentre com o numero decimal:");
     scanf("%d", &n);
     d = n;
     i = 0;
     while (d >= 2)
     {
-        resto[i] = d % 2;
+        restos[i] = d % 2;
         d = d / 2;
         i++;
     }
-    resto[i] = d;
+    restos[i] = d;
     printf("\n\n % d decimal em binario =", n);
     for (j = i; j >= 0; j--)
     {
-        printf("%d", resto[j]);
+        printf("%d", restos[j]);
 
         printf("\n\nfim\n\n");
         system("pause");
