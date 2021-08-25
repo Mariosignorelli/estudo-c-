fa82efd570a188b031b1e41d3c444906dfb5f539
signorellidevelopers <66884792+signorellidevelopers@users.noreply.github.com>
2021-08-25 20:29:27 -0300

    atulizado

--- pratica-prog-c++/vetor-pratica-med.cpp
+++ pratica-prog-c++/vetor-pratica-med.cpp
@@ -4,19 +4,18 @@ main()
 {
     float nota[5];
     int i;
+    float soma, media;
     i = 0;
+    soma = 0;
     while (i < 5)
     {
         printf("Digite %d nota", i + 1);
         scanf("%f", &nota[i]);
+        soma = soma+nota[i];
         i++;
     }
-    printf("\n\nas notas digitadas no vetor foram\n\n");
-    i = 0;
-    while (i < 5)
-    {
-        printf("%f",nota[i]);
-        i++;
-    }
+    media = soma/5;
+    printf("\n\n media e:%f\n\n", media);
+   
     system("pause");
 }
