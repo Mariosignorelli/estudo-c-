874ee2c1900b22081fd30ca190d82e1b4fa9cfc1
signorellidevelopers <66884792+signorellidevelopers@users.noreply.github.com>
2021-09-03 19:00:58 -0300

    seguindo com estudo c++

---
+++ pratica-prog-c++/contante/programação-estruturado/parametros.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void quadrado(int x){
+    x = x*x;
+    printf("\n o valor de x = %d",x);
+}
+main(){
+    int x = 8;
+    quadrado (x);
+     printf("\n o valor de x = %d ",x);
+     system("PAUSE");
+
+}
+
+
