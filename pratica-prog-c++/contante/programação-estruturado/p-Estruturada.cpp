ba036c8358aab7efc6cf45b15497409e65853f75
signorellidevelopers <66884792+signorellidevelopers@users.noreply.github.com>
2021-09-03 18:25:58 -0300

    atualizado

--- pratica-prog-c++/contante/programação-estruturado/p-Estruturada.cpp
+++ pratica-prog-c++/contante/programação-estruturado/p-Estruturada.cpp
@@ -1,81 +1,87 @@
-#include<stdio.h>
+#include<stdio.h> 
 #include<stdlib.h>
 #include<conio.h>
-
-float A, B, R;
-    void entrada(){
-        printf("\nDigite o Primeiro número:");
-        scanf("%f",&A);
-        printf("\nDigite o segundo número:");
-        scanf("%f",&B);
-    }
-    
-    
-        float Adicao (){
-            R = A + B;
-            return(R);
-        }
-        float Sutracao (){
-            R = A - B;
-            return(R);
-        }
-        float Multiplicacao (){
-            R = A * B;
-            return(R);
-        }
-        float Divisao (){
-            R = A / B;
-            return(R);
-        }
-                main ()
-            {
-            int opcao=0;
-            float result;
-            
-            while (opcao!=5)
-            {
-            system("cls");
-            printf("\n1-adicao");         
-            printf("\n2-subtracao");   
-            printf("\n3-multiplicacao");  
-            printf("\n4-divisao");   
-            printf("\n5-sair");
-            printf("\n\n-Escolha um num:");
-            scanf("%d",&opcao);
   
-                switch(opcao)
-                {
-                    case 1:
-                    {     
-                        Entrada ();
-                        result = Adicao ();
-                        printf ("\n A + B = %f", result);
-                        break;  
-                    }          
-                case 2:
-                    {     
-                        Entrada ();
-                        result = Subtracao ();
-                        printf ("\n A - B = %f", result);
-                        break;  
-                    }  
-                case 3:
-                    {     
-                        Entrada ();
-                        result = Multiplicacao ();
-                        printf ("\n A * B = %f", result);
-                        break;  
-                    }          
-                    case 4:
-                    {     
-                        Entrada ();
-                        result = Divisao ();
-                        printf ("\n A / B = %f", result);
-                        break;  
-                    }          
-                
-                    default:printf("\nEsta operacao nao existe");
-                } 
-                }
-                system("PAUSE");                           
-                }
+float A, B, R;
+ 
+void Entrada ( )
+{
+       printf("\nDigite o primeiro numero: ");
+       scanf("%f",&A);
+       printf("\nDigite o segundo numero: ");
+       scanf("%f",&B);    
+}
+ 
+float  Adicao ( )
+{
+       R = A + B;
+       return (R);
+}
+float  Subtracao ( )
+{
+       R = A - B;
+       return (R);
+}
+float  Multiplicacao ( )
+{
+       R = A * B;
+       return (R);
+}
+float  Divisao ( )
+{
+       R = A / B;
+       return (R);
+}
+ 
+main ()
+{
+  int opcao=0;
+  float result;
+ 
+  while (opcao!=5)
+  {
+  system("cls");
+  printf("\n1-adicao");         
+  printf("\n2-subtracao");   
+  printf("\n3-multiplicacao");  
+  printf("\n4-divisao");   
+  printf("\n5-sair");
+  printf("\n\n-Escolha um num:");
+  scanf("%d",&opcao);
+  
+  switch(opcao)
+   {
+     case 1:
+     {     
+          Entrada ();
+          result = Adicao ();
+          printf ("\n A + B = %f", result);
+          break;  
+     }          
+case 2:
+     {     
+          Entrada ();
+          result = Subtracao ();
+          printf ("\n A - B = %f", result);
+          break;  
+     }  
+case 3:
+     {     
+          Entrada ();
+          result = Multiplicacao ();
+          printf ("\n A * B = %f", result);
+          break;  
+     }          
+      case 4:
+     {     
+          Entrada ();
+          result = Divisao ();
+          printf ("\n A / B = %f", result);
+          break;  
+     }          
+ 
+     default:printf("\nEsta operacao nao existe");
+   } 
+   }
+system("PAUSE");                           
+}
